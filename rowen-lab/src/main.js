a0a7eb8c6ff7fc365764909aefb4fd9f43fd8cf4
Rowen Scout <rowen.scout@gmail.com>
2018-02-17 12:47:00 -0800

    having some errors, but I got everything to render at least.

---
+++ rowen-lab/src/main.js
@@ -0,0 +1,57 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/main.scss';
+import Form from './component/app/note/form.js';
+import List from './component/app/note/list.js';
+
+
+const main = document.getElementById('root');
+
+class Apps extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {notes: []};
+  }
+
+  updateNote(id, newContent) {
+
+    this.state.notes.forEach((note,i) => {
+      if (note.id == id) this.state.notes[i].content = newContent;
+    });
+    this.setState(this.state);
+  }
+
+  editNote(id) {
+
+    this.state.notes.forEach((note,i) => {
+      if (note.id == id) this.state.notes[i].editing = !this.state.notes[i].editing;
+    });
+      this.setState(this.state);
+    }
+
+    addNote(note) {
+      this.setState({
+        notes: [...this.state.notes, {...note}]
+      });
+    }
+
+    deleteNote(_id) {
+      this.setState( {
+        notes: [...this.state.notes].filter(note => note.id !== _id)
+      });
+    }
+
+  render() {
+    return (
+      <div>
+        <h1>'Notes: '</h1>
+        <Form addNote={this.addNote} />
+        <List notes={this.state.notes} edit={this.editNote} newContent={this.updateNote} deleteContent={this.deleteNote}/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Apps/>, main);
